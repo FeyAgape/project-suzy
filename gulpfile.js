426088272ed3ea0652f7ad3c7f56f6ad72d0c3df
Fey Ijaware <Fey.ijaware@talent-works.com>
2017-10-12 16:02:53 +0100

    updated file directory

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+//gulp.task('task-name', function() {
+  // task-name refers to the name of the task, which would be used whenever you want to run a task in Gulp. 
+  //You can also run the same task in the command line by writing gulp task-name
+//});
+
+gulp.task('hello', function() {
+  console.log('Hello World');
+});
