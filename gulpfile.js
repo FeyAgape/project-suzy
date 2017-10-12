e7ef2da3d7ef4d890b38496c4bbe59631314296f
Fey Ijaware <Fey.ijaware@talent-works.com>
2017-10-12 16:17:48 +0100

    complied a scss file into css

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,27 @@ var gulp = require('gulp');
 gulp.task('hello', function() {
   console.log('Hello World');
 });
+
+/*gulp.task('task-name', function () {
+  return gulp.src('source-files') // Get source files with gulp.src
+    .pipe(aGulpPlugin()) // Sends it through a gulp plugin
+    .pipe(gulp.dest('destination')) // Outputs the file in the destination folder
+})*/
+
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+/*gulp.task('sass', function(){
+  return gulp.src('source-files')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('destination'))
+});*/
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
+
+// run gulp sass in the command line, you should now be able to see that a styles.css 
