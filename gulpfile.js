1715a21872611c5a2379aa40e22a3137a29b0d44
Fey Ijaware <Fey.ijaware@talent-works.com>
2017-10-13 09:58:45 +0100

    updated to include plugins for optimizing css and Js

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,42 @@ gulp.task('browserSync', function() {
   })
 })
 
-gulp.task('watch',['browserSync'], function(){ //telling the watch task that browserSync must be completed before watch is allowed to run.
+gulp.task('watch',['browserSync', 'sass'], function(){ //telling the watch task that browserSync and sass runs before watch is allowed to run.
   gulp.watch('app/scss/**/*.scss', ['sass']); 
   // Other watchers
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 })
+
+var useref = require('gulp-useref'); /*Gulp-useref concatenates any number of CSS and JavaScript 
+files into a single file by looking for a comment that starts with "<!--build:" and ends with "<!--endbuild-->"*/
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+}); //run gulp useref in the cmd to run
+
+// Other requires...
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+var cssnano = require('gulp-cssnano');
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
