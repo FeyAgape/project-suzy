e7fdf128e06b98fe20ea52451d27f58f5ad03470
Fey Ijaware <Fey.ijaware@talent-works.com>
2018-05-18 10:54:36 +0100

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
+// Requiring autoprefixer
+var autoprefixer = require('gulp-autoprefixer');
 
 
 //this task just prints Hello Fey! when runned 
@@ -11,12 +13,13 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
   //return gulp.src('app/scss/styles.scss') // this only lets use compile .scss file
   // using globbing allow us to compile more than one
-   return gulp.src('app/scss/**/*.scss') // using Globbing gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('app/scss/**/*.scss') // using Globbing gets all files ending with .scss in app/scss and children dirs
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(autoprefixer()) // Passes it through gulp-autoprefixer
     .pipe(gulp.dest('app/css'))
 });
 
-// Gulp watch syntax
+// Gulp watch syntax, run gulp watch in the command line to activate
 //gulp.watch('app/scss/**/*.scss', ['sass']); // this only allows us to watch one type of file
 //to watch multi tasks we can group together multiple watch processes into a watch task
 gulp.task('watch', function(){
